2ef9b621af66a45b0d9bca227a03d96ceb6b45e8
Divakaran Baskaran <divakaran@arrcus.com>
2025-12-04 02:17:26 +0530

    feat: Add SR-MPLS support to ISIS config parser with adjacency-sid, prefix-sid, and TI-LFA SR-MPLS parsing
    - Add `ti_lfa_sr_mpls_enabled` field to fast-reroute schema for TI-LFA SR-MPLS configuration
    - Add `adjacency_sids` list field to afi-safi schema for per-neighbor adjacency SID configuration
    - Add `prefix_sids` list field to afi-safi schema for per-algorithm prefix SID configuration
    - Parse adjacency-sid entries with neighbor, sid-type, and value fields
    - Parse prefix-sid entries with algorithm

--- src/genie/libs/parser/arcos/constants.py
+++ src/genie/libs/parser/arcos/constants.py
@@ -6,6 +6,7 @@ used by the ArcOS parsers.
 
 # Namespace prefixes
 ARCOS_ISIS_AUGMENTS = "arcos-openconfig-isis-augments"
+ARCOS_SR_AUGMENTS = "arcos-openconfig-segment-routing-augments"
 OPENCONFIG_INTERFACES = "openconfig-interfaces:interfaces"
 OPENCONFIG_NETWORK_INSTANCES = "openconfig-network-instance:network-instances"
 ARCOS_SRV6 = "arcos-srv6:srv6"
